1a64dcc0ee051110789255c4362eb02e50529d95
Ignatius Bagus <ignatius.mbs@gmail.com>
2019-05-17 02:31:24 +0700

    Use standard, remove semicolons

--- code.js
+++ code.js
@@ -1,82 +1,82 @@
 const bshCreateToolbar = () => {
-  const toolbar = document.createElement('div');
-  toolbar.className = 'cb-toolbar';
+  const toolbar = document.createElement('div')
+  toolbar.className = 'cb-toolbar'
 
   const createTool = (iconClass, text) => {
-    let tool = document.createElement('a');
-    let icon = document.createElement('i');
-    let tooltip = document.createElement('span');
-    tool.className = `tb-item`;
-    icon.className = iconClass;
-    tooltip.className = 'tb-tooltip';
-    tooltip.innerText = text;
-    tool.appendChild(icon);
-    tool.appendChild(tooltip);
-    return tool;
-  };
+    let tool = document.createElement('a')
+    let icon = document.createElement('i')
+    let tooltip = document.createElement('span')
+    tool.className = `tb-item`
+    icon.className = iconClass
+    tooltip.className = 'tb-tooltip'
+    tooltip.innerText = text
+    tool.appendChild(icon)
+    tool.appendChild(tooltip)
+    return tool
+  }
 
-  const copyButton = createTool('far fa-copy', 'Copy');
+  const copyButton = createTool('far fa-copy', 'Copy')
   copyButton.addEventListener('click', e => {
-    const clickedTool = e.currentTarget;
-    const currentBox = clickedTool.parentElement.parentElement;
-    const codeLines = currentBox.getElementsByTagName('code');
+    const clickedTool = e.currentTarget
+    const currentBox = clickedTool.parentElement.parentElement
+    const codeLines = currentBox.getElementsByTagName('code')
 
-    const copyArea = document.createElement('textarea');
-    copyArea.className = 'ghost-area';
+    const copyArea = document.createElement('textarea')
+    copyArea.className = 'ghost-area'
     for (let j = 0; j < codeLines.length; j++) {
-      if (j !== codeLines.length - 1) copyArea.value += codeLines[j].innerText + '\n';
-      else copyArea.value += codeLines[j].innerText;
+      if (j !== codeLines.length - 1) copyArea.value += codeLines[j].innerText + '\n'
+      else copyArea.value += codeLines[j].innerText
     }
-    copyArea.value.trim();
-    document.body.appendChild(copyArea);
-    copyArea.focus();
-    copyArea.select();
+    copyArea.value.trim()
+    document.body.appendChild(copyArea)
+    copyArea.focus()
+    copyArea.select()
     try {
-      if (document.execCommand('copy')) alert('Code Block Copied!');
-      else alert('Copy Unsuccessful...');
+      if (document.execCommand('copy')) alert('Code Block Copied!')
+      else alert('Copy Unsuccessful...')
     } catch (err) {
-      alert('Error, unable to copy');
+      alert('Error, unable to copy')
     }
-    document.body.removeChild(copyArea);
-  });
-  toolbar.appendChild(copyButton);
+    document.body.removeChild(copyArea)
+  })
+  toolbar.appendChild(copyButton)
 
-  const toggleNumbering = createTool('fas fa-list-ol', 'Toggle Numbering');
+  const toggleNumbering = createTool('fas fa-list-ol', 'Toggle Numbering')
   toggleNumbering.addEventListener('click', e => {
-    const clickedTool = e.currentTarget;
-    const currentBox = clickedTool.parentElement.parentElement;
-    currentBox.classList.toggle('numbered');
-  });
-  toolbar.appendChild(toggleNumbering);
+    const clickedTool = e.currentTarget
+    const currentBox = clickedTool.parentElement.parentElement
+    currentBox.classList.toggle('numbered')
+  })
+  toolbar.appendChild(toggleNumbering)
 
-  return toolbar;
-};
+  return toolbar
+}
 
 for (const codeBox of document.getElementsByClassName('code-box')) {
-  const header = codeBox.getElementsByClassName('cb-header');
+  const header = codeBox.getElementsByClassName('cb-header')
   if (header.length > 0) {
     for (const head of header) {
-      if (head.innerHTML.length === 0) head.classList.add('none');
-      const dataLang = head.dataset.language;
+      if (head.innerHTML.length === 0) head.classList.add('none')
+      const dataLang = head.dataset.language
       // pre tag boxes
-      const pres = head.parentElement.getElementsByTagName('pre');
+      const pres = head.parentElement.getElementsByTagName('pre')
       if (dataLang !== undefined && dataLang !== '') {
-        for (const pre of pres) pre.classList.add(`language-${dataLang}`);
+        for (const pre of pres) pre.classList.add(`language-${dataLang}`)
       } else {
-        for (const pre of pres) pre.classList.add('language-none');
+        for (const pre of pres) pre.classList.add('language-none')
       }
       // data lines
       for (const pre of pres) {
-        const codes = pre.getElementsByTagName('code');
-        let dataLine = pre.dataset.line;
+        const codes = pre.getElementsByTagName('code')
+        let dataLine = pre.dataset.line
         for (const num in codes) {
           if (codes.hasOwnProperty(num)) {
-            if (dataLine === undefined) dataLine = 1;
-            codes[num].dataset.line = parseInt(dataLine, 10) + parseInt(num);
+            if (dataLine === undefined) dataLine = 1
+            codes[num].dataset.line = parseInt(dataLine, 10) + parseInt(num)
           }
         }
         // code toolbar
-        pre.insertAdjacentElement('afterend', bshCreateToolbar());
+        pre.insertAdjacentElement('afterend', bshCreateToolbar())
       }
     }
   }
