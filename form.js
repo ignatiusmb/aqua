8e258e77823b7a5a9b18a1dc34b77dc06ad3b15e
Ignatius Bagus <ignatius.mbs@gmail.com>
2019-05-21 01:04:59 +0700

    Add form bluesheet

---
+++ form.js
@@ -0,0 +1,20 @@
+for (const form of document.getElementsByClassName('bsh-form')) {
+  if (form.tagName === 'FORM') {
+    for (const input of form.getElementsByTagName('input')) {
+      const label = document.createElement('label')
+      const newInput = document.createElement('input')
+      const placeholder = document.createElement('span')
+
+      label.htmlFor = input.type
+      newInput.type = input.type
+      newInput.placeholder = ' '
+      placeholder.textContent = input.placeholder
+      placeholder.textContent.charAt(0).toUpperCase()
+
+      label.appendChild(newInput)
+      label.appendChild(placeholder)
+      input.insertAdjacentElement('afterend', label)
+      form.removeChild(input)
+    }
+  }
+}
