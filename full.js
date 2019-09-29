17b2f00c68b2453ab040fbded5b70e21d51a5e5f
Ignatius Bagus <ignatius.mbs@gmail.com>
2019-09-29 15:41:03 +0700

    Merge branch 'master' of https://github.com/ignatiusmb/bluesheets into gh-pages

--- full.js
+++ full.js
@@ -1,12 +1,25 @@
-window.addEventListener('DOMContentLoaded', () => {
-  const scriptNames = ['code', 'form', 'modal']
-  const frag = document.createDocumentFragment()
-  const addBlueScript = name => {
-    const script = document.createElement('script')
-    script.type = 'text/javascript'
-    script.src = `https://cdn.jsdelivr.net/gh/ignatiusmb/bluesheets/${name}.min.js`
-    return script
+const filenames = ['code', 'form', 'modal']
+const frag = document.createDocumentFragment()
+const addBlueSheet = (name, styling) => {
+  let tag
+  if (styling) {
+    tag = document.createElement('link')
+    tag.rel = 'stylesheet'
+    tag.type = 'text/css'
+    tag.href = `https://cdn.jsdelivr.net/gh/ignatiusmb/bluesheets/${name}.css`
+  } else {
+    tag = document.createElement('script')
+    tag.type = 'text/javascript'
+    tag.src = `https://cdn.jsdelivr.net/gh/ignatiusmb/bluesheets/${name}.js`
   }
-  for (const name of scriptNames) frag.appendChild(addBlueScript(name))
-  document.body.insertAdjacentElement('beforeend', frag)
+  return tag
+}
+window.addEventListener('DOMContentLoaded', () => {
+  frag.appendChild('core', true)
+  for (const name of filenames) frag.appendChild(addBlueSheet(name, true))
+  document.head.appendChild(frag)
+})
+window.addEventListener('load', () => {
+  for (const name of filenames) frag.appendChild(addBlueSheet(name, false))
+  document.body.appendChild(frag)
 })
