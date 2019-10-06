203e15e78ee2ccbe8c670781850dc83503beefe0
Ignatius Bagus <ignatius.mbs@gmail.com>
2019-10-07 01:09:23 +0700

    Optimize form bluescript

--- form.js
+++ form.js
@@ -1,20 +1,16 @@
-for (const form of document.getElementsByClassName('bss-form')) {
-  if (form.tagName === 'FORM') {
-    for (const input of form.getElementsByTagName('input')) {
-      const label = document.createElement('label')
-      const newInput = document.createElement('input')
-      const placeholder = document.createElement('span')
+for (const form of document.querySelectorAll('form.bss-form')) {
+  for (const input of form.querySelectorAll('input')) {
+    const label = document.createElement('label')
+    const inputChange = document.createElement('input')
+    const placeholder = document.createElement('span')
 
-      label.htmlFor = input.type
-      newInput.type = input.type
-      newInput.placeholder = ' '
-      placeholder.textContent = input.placeholder
-      placeholder.textContent.charAt(0).toUpperCase()
+    label.htmlFor = inputChange.type = input.type
+    inputChange.placeholder = ' '
+    placeholder.textContent = input.placeholder
+    placeholder.textContent.charAt(0).toUpperCase()
 
-      label.appendChild(newInput)
-      label.appendChild(placeholder)
-      input.insertAdjacentElement('afterend', label)
-      form.removeChild(input)
-    }
+    label.appendChild(inputChange)
+    label.appendChild(placeholder)
+    input.replaceWith(label)
   }
 }
