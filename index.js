8c7e89d454b0622fa1f8b9553bef2cd0af893ca6
Ignatius Bagus <ignatius.mbs@gmail.com>
2019-11-06 13:12:33 +0700

    Add index.js main file

---
+++ index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  code: { style: require('./lib/code.css'), script: require('./lib/code') },
+  form: { style: require('./lib/form'), script: require('./lib/form') },
+  code: { style: require('./lib/code.css'), script: require('./lib/code') },
+  modal: { style: require('./lib/modal.css'), script: require('./lib/modal') }
+}
