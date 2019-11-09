5a2f243dea7c5bcd34ab3e617107562701b64892
Ignatius Bagus <ignatius.mbs@gmail.com>
2019-11-09 17:27:33 +0700

    Create multiple compilation options

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
 const fs = require('fs')
 const del = require('del')
+
 const gulp = require('gulp')
 const header = require('gulp-header')
+const concat = require('gulp-concat')
+const cssimport = require('gulp-cssimport')
 const clean = require('gulp-clean-css')
 const uglify = require('gulp-uglify-es').default
 
@@ -15,38 +18,58 @@ const banner = `/*! Aqua v${pkg.version} by @ignatiusmb - imbagus.com
 console.log(pkg.version)
 const paths = {
   styles: {
-    src: './src/**/*.css',
-    dst: './lib/'
+    core: './src/core.css',
+    main: ['./src/*.css', '!./src/core.css'],
+    extra: ['./src/**/*.css', '!./src/*.css']
   },
   scripts: {
-    src: './src/**/*.js',
-    dst: './lib/'
-  }
+    main: './src/index.js',
+    extra: ['./src/*/*.js', '!./src/*.js']
+  },
+  dst: './lib/'
 }
 
 const reset = () => del(['lib'])
-const styles = () =>
-  gulp
-    .src(paths.styles.src)
+
+const styles = done => {
+  gulp // core style
+    .src(paths.styles.core)
     .pipe(clean())
     .pipe(header(banner))
-    .pipe(gulp.dest(paths.scripts.dst))
-const scripts = () =>
-  gulp
-    .src(paths.scripts.src)
-    .pipe(uglify())
+    .pipe(gulp.dest(paths.dst))
+
+  gulp // main modules
+    .src(paths.styles.main)
+    .pipe(cssimport())
+    .pipe(clean())
+    .pipe(concat('main.css'))
     .pipe(header(banner))
-    .pipe(gulp.dest(paths.scripts.dst))
-const watch = () => {
-  gulp.watch(paths.styles.src, styles)
-  gulp.watch(paths.scripts.src, scripts)
+    .pipe(gulp.dest(paths.dst))
+
+  gulp // extra modules
+    .src(paths.styles.extra)
+    .pipe(cssimport())
+    .pipe(clean())
+    .pipe(header(banner))
+    .pipe(gulp.dest(paths.dst))
+
+  done()
 }
+
+const scripts = done => {
+  gulp
+    .src(paths.scripts.main)
+    .pipe(header(banner))
+    .pipe(gulp.dest(paths.dst))
+
+  done()
+}
+
 const build = gulp.series(reset, gulp.parallel(styles, scripts))
 
 exports.reset = reset
 exports.styles = styles
 exports.scripts = scripts
-exports.watch = watch
 exports.build = build
 
 exports.default = build
