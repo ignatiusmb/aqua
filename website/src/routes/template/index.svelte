ca4a6609adaaa26652907177ee7685c9a73f6533
Ignatius Bagus <ignatius.mbs@gmail.com>
2019-11-10 17:18:23 +0700

    Add noscroll on click

--- website/src/routes/template/index.svelte
+++ website/src/routes/template/index.svelte
@@ -17,7 +17,7 @@
       <div class="card">
         <h1>Form &rarr; Login</h1>
 
-        <a href="/template/login">
+        <a href="/template/login" sapper-noscroll>
           <button>See Template</button>
         </a>
       </div>
