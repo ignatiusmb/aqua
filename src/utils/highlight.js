0cbcca41b85a7d2f52c3b36b03f755a3f2bd180e
Ignatius Bagus <ignatius.mbs@gmail.com>
2020-06-16 18:57:22 +0700

    Add imported languages to langMap dict

--- src/utils/highlight.js
+++ src/utils/highlight.js
@@ -5,6 +5,7 @@ import 'prismjs/components/prism-cpp';
 import 'prismjs/components/prism-css';
 import 'prismjs/components/prism-dart';
 import 'prismjs/components/prism-docker';
+import 'prismjs/components/prism-markup';
 import 'prismjs/components/prism-java';
 import 'prismjs/components/prism-javascript';
 import 'prismjs/components/prism-python';
@@ -15,14 +16,23 @@ import './prism-svelte';
 
 const langMap = {
 	bash: 'bash',
+	c: 'c',
+	'c++': 'cpp',
+	cpp: 'cpp',
 	css: 'css',
 	dart: 'dart',
 	docker: 'docker',
 	html: 'markup',
+	java: 'java',
 	javascript: 'javascript',
 	js: 'javascript',
+	py: 'python',
+	python: 'python',
 	sv: 'svelte',
 	svelte: 'svelte',
+	sql: 'sql',
+	ts: 'typescript',
+	typescript: 'typescript',
 	yaml: 'yaml',
 };
 
