e533f70f622cb6c7751b0a21a7b685ba2d807dff
Ignatius Bagus <ignatius.mbs@gmail.com>
2020-07-09 23:20:16 +0700

    Add GitHub Link to repository

--- docs/src/components/Header.svelte
+++ docs/src/components/Header.svelte
@@ -1,30 +1,36 @@
 <script>
-  import { version } from '../../../package.json';
+	import Link from '../svelte/Link.svelte';
+	import Icon from '../svelte/Icon.svelte';
+	import { version } from '../../../package.json';
 </script>
 
 <header>
-  <h1>Aqua</h1>
-  <h3>Version v{version}</h3>
+	<h1>Aqua</h1>
+	<h3>Version v{version}</h3>
+	<Link href="https://github.com/ignatiusmb/aqua/" inherit invert>
+		<Icon name="github" size="34" />
+	</Link>
 </header>
 
 <style>
-  header {
-    height: 100vh;
-    display: grid;
-    gap: 1em;
-    align-content: center;
-    background: #0082c8;
-    background: -webkit-linear-gradient(to right, #000046, #1cb5e0);
-    background: linear-gradient(to right, #000046, #1cb5e0);
-    text-align: center;
-    color: white;
-  }
-  h1 {
-    font-size: 4rem;
-    font-family: var(--aqua-heading);
-  }
-  h3 {
-    font-size: 1.5rem;
-    font-family: var(--aqua-monospace);
-  }
+	header {
+		height: 100vh;
+		display: grid;
+		gap: 1em;
+		align-content: center;
+		justify-content: center;
+		background: #0082c8;
+		background: -webkit-linear-gradient(to right, #000046, #1cb5e0);
+		background: linear-gradient(to right, #000046, #1cb5e0);
+		text-align: center;
+		color: white;
+	}
+	h1 {
+		font-size: 4rem;
+		font-family: var(--aqua-heading);
+	}
+	h3 {
+		font-size: 1.5rem;
+		font-family: var(--aqua-monospace);
+	}
 </style>
