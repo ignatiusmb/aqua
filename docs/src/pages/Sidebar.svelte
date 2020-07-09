9e4f8f3017819f9ba62b6124c4105fd26ce884b5
Ignatius Bagus <ignatius.mbs@gmail.com>
2020-07-09 14:26:16 +0700

    Add documentation components and routes

---
+++ docs/src/pages/Sidebar.svelte
@@ -0,0 +1,33 @@
+<script>
+	export let sections;
+</script>
+
+<aside>
+	{#each sections as { slug, title }, i}
+		<a href="#{slug}" data-index={i + 1}>{title}</a>
+	{/each}
+</aside>
+
+<style>
+	aside {
+		position: sticky;
+		top: 0;
+		display: none;
+		align-content: flex-start;
+		padding-top: 2em;
+	}
+	a {
+		line-height: 4ch;
+	}
+	a::before {
+		content: attr(data-index);
+		margin-right: 0.75em;
+		font-family: var(--aqua-monospace);
+	}
+
+	@media only screen and (min-width: 769px) {
+		aside {
+			display: grid;
+		}
+	}
+</style>
---
+++ docs/src/routes/index.svelte
@@ -0,0 +1,33 @@
+<script context="module">
+	export async function preload() {
+		return { sections: await this.fetch('docs.json').then((r) => r.json()) };
+	}
+</script>
+
+<script>
+	export let sections;
+	import MetaHead from '../pages/MetaHead.svelte';
+	import Wrapper from '../pages/Wrapper.svelte';
+	import Sidebar from '../pages/Sidebar.svelte';
+	import Section from '../pages/Section.svelte';
+</script>
+
+<MetaHead
+	title="Aqua - Native DOM Enhancements"
+	description="Aqua by Ignatius. An npm package to quickly build websites with predefined styles and components." />
+
+<Wrapper>
+	<Sidebar {sections} />
+
+	<article>
+		{#each sections as docs}
+			<Section {docs} />
+		{/each}
+	</article>
+</Wrapper>
+
+<style>
+	article {
+		padding-top: 2em;
+	}
+</style>
