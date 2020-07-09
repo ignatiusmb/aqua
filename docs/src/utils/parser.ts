206355669f7cf142c8ad4313bdc4d80242b598e7
Ignatius Bagus <ignatius.mbs@gmail.com>
2020-07-10 01:29:58 +0700

    Add and use bundled output in docs

--- docs/src/utils/parser.ts
+++ docs/src/utils/parser.ts
@@ -1,7 +1,7 @@
 import { join } from 'path';
 import { readdirSync, readFileSync } from 'fs';
 import { sortCompare, splitAt } from './helper';
-import Aqua from '@ignatiusmb/aqua';
+import Aqua from '../aqua';
 const markIt = require('markdown-it')({
 	html: true,
 	typographer: true,
