58c7a794d0888249a8666568fc8f2c606d226b87
Ignatius Bagus <ignatius.mbs@gmail.com>
2020-07-09 14:25:21 +0700

    Add configuration files

---
+++ docs/rollup.config.js
@@ -0,0 +1,119 @@
+import babel from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import config from 'sapper/config/rollup.js';
+import json from '@rollup/plugin-json';
+import pkg from './package.json';
+import replace from '@rollup/plugin-replace';
+import resolve from '@rollup/plugin-node-resolve';
+import svelte from 'rollup-plugin-svelte';
+import typescript from '@rollup/plugin-typescript';
+import { terser } from 'rollup-plugin-terser';
+
+const mode = process.env.NODE_ENV;
+const dev = mode === 'development';
+const legacy = !!process.env.SAPPER_LEGACY_BUILD;
+
+const onwarn = (warning, onwarn) =>
+	(warning.code === 'CIRCULAR_DEPENDENCY' && /[/\\]@sapper[/\\]/.test(warning.message)) || onwarn(warning);
+
+export default {
+	client: {
+		input: config.client.input(),
+		output: config.client.output(),
+		plugins: [
+			replace({
+				'process.browser': true,
+				'process.env.NODE_ENV': JSON.stringify(mode),
+			}),
+			svelte({
+				dev,
+				hydratable: true,
+				emitCss: true,
+			}),
+			resolve({
+				browser: true,
+				dedupe: ['svelte'],
+			}),
+			commonjs(),
+			json(),
+
+			legacy &&
+				babel({
+					extensions: ['.js', '.mjs', '.html', '.svelte'],
+					babelHelpers: 'runtime',
+					exclude: ['node_modules/@babel/**'],
+					presets: [
+						[
+							'@babel/preset-env',
+							{
+								targets: '> 0.25%, not dead',
+							},
+						],
+					],
+					plugins: [
+						'@babel/plugin-syntax-dynamic-import',
+						[
+							'@babel/plugin-transform-runtime',
+							{
+								useESModules: true,
+							},
+						],
+					],
+				}),
+
+			!dev &&
+				terser({
+					module: true,
+				}),
+		],
+
+		preserveEntrySignatures: false,
+		onwarn,
+	},
+
+	server: {
+		input: config.server.input(),
+		output: config.server.output(),
+		plugins: [
+			replace({
+				'process.browser': false,
+				'process.env.NODE_ENV': JSON.stringify(mode),
+			}),
+			svelte({
+				dev,
+				hydratable: true,
+				generate: 'ssr',
+			}),
+			resolve({
+				dedupe: ['svelte'],
+			}),
+			commonjs(),
+			json(),
+			typescript(),
+		],
+		external: Object.keys(pkg.dependencies).concat(
+			require('module').builtinModules || Object.keys(process.binding('natives'))
+		),
+
+		preserveEntrySignatures: 'strict',
+		onwarn,
+	},
+
+	serviceworker: {
+		input: config.serviceworker.input(),
+		output: config.serviceworker.output(),
+		plugins: [
+			resolve(),
+			replace({
+				'process.browser': true,
+				'process.env.NODE_ENV': JSON.stringify(mode),
+			}),
+			commonjs(),
+			json(),
+			!dev && terser(),
+		],
+
+		preserveEntrySignatures: false,
+		onwarn,
+	},
+};
