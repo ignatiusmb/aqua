cf044fe4b9c0a5f767df45d97e28e1e6d2f09572
Ignatius Bagus <ignatius.mbs@gmail.com>
2020-07-09 14:27:37 +0700

    Fix content overflowing with grid

--- docs/src/pages/Wrapper.svelte
+++ docs/src/pages/Wrapper.svelte
@@ -6,13 +6,15 @@
 	main {
 		max-width: 84em;
 		display: grid;
-		gap: 1em;
-		padding: 2em;
+		gap: 2em;
+		grid-template-columns: minmax(0, 1fr);
+		align-items: flex-start;
+		padding: 0 2em;
 		margin: 2em auto;
 	}
 	@media only screen and (min-width: 769px) {
 		main {
-			grid-template-columns: minmax(12em, 25%) 1fr;
+			grid-template-columns: minmax(12em, 20%) minmax(0, 1fr);
 		}
 	}
 </style>
