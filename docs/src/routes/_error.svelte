f78f90f2e974a37bd6a6014b3c035417de3a68e8
Ignatius Bagus <ignatius.mbs@gmail.com>
2020-08-02 19:59:27 +0700

    Change space to tabs

--- docs/src/routes/_error.svelte
+++ docs/src/routes/_error.svelte
@@ -1,42 +1,42 @@
 <script>
-  export let status, error;
+	export let status, error;
 
-  const dev = process.env.NODE_ENV === 'development';
+	const dev = process.env.NODE_ENV === 'development';
 </script>
 
 <svelte:head>
-  <title>{status}</title>
+	<title>{status}</title>
 </svelte:head>
 
 <article>
-  <section>
-    <h1>{status}</h1>
-    <p>{error.message}</p>
-  </section>
+	<section>
+		<h1>{status}</h1>
+		<p>{error.message}</p>
+	</section>
 
-  {#if dev && error.stack}
-    <section>
-      <pre>{error.stack}</pre>
-    </section>
-  {/if}
+	{#if dev && error.stack}
+		<section>
+			<pre>{error.stack}</pre>
+		</section>
+	{/if}
 </article>
 
 <style>
-  section {
-    height: 100vh;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-  }
-  section:first-of-type {
-    text-align: center;
-  }
+	section {
+		height: 100vh;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+	section:first-of-type {
+		text-align: center;
+	}
 
-  h1 {
-    font-size: 5em;
-  }
-  p {
-    font-size: 2em;
-  }
+	h1 {
+		font-size: 5em;
+	}
+	p {
+		font-size: 2em;
+	}
 </style>
