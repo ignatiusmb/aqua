921c26f4c319931ed1b4e3aafe9b8609208f8e56
Ignatius Bagus <ignatius.mbs@gmail.com>
2020-10-20 17:28:59 +0700

    feat: change version to be replaceable in docs.json

--- docs/src/routes/docs.json.ts
+++ docs/src/routes/docs.json.ts
@@ -1,11 +1,13 @@
 import type { Response } from 'express';
 import { parseDir } from '../utils/parser';
 import { splitAt } from '../utils/helper';
+import { version } from '../package';
 
 export function get(_, res: Response) {
 	const docs = parseDir('content', (data, content, filename) => {
 		const [index, slug] = splitAt(2, filename.split('.')[0]);
 		const path = `docs/content/${index}-${slug}.md`;
+		content = content.replace(/@VERSION/g, `@${version}`);
 		return { index, slug, ...data, content, path };
 	}).sort((x, y) => x.index - y.index);
 
