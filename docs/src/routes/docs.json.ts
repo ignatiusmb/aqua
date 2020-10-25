e72a08a7510a2ea47fbcc1f206265d5e39825a67
Ignatius Bagus <ignatius.mbs@gmail.com>
2020-10-25 14:58:00 +0700

    docs: generate version dynamically (#28)

--- docs/src/routes/docs.json.ts
+++ docs/src/routes/docs.json.ts
@@ -1,13 +1,15 @@
-import type { Response } from 'express';
+import type { Request, Response } from 'express';
 import { parseDir } from '../utils/parser';
 import { splitAt } from '../utils/helper';
+import { version } from '../package';
 
-export function get(_, res: Response) {
-	const docs = parseDir('content', (data, content, filename) => {
+export function get(_: Request, res: Response) {
+	const docs = parseDir('content', (data: any, content: string, filename: string) => {
 		const [index, slug] = splitAt(2, filename.split('.')[0]);
 		const path = `docs/content/${index}-${slug}.md`;
+		content = content.replace(/@VERSION/g, `@${version}`);
 		return { index, slug, ...data, content, path };
-	}).sort((x, y) => x.index - y.index);
+	}).sort((x: any, y: any) => x.index - y.index);
 
 	res.writeHead(200, { 'Content-Type': 'application/json' });
 	res.end(JSON.stringify(docs));
