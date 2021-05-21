8037fd97155fe1a401b8cf84be5bacf35599e8dd
Ignatius Bagus <ignatius.mbs@gmail.com>
2021-05-21 22:41:37 +0700

    update sveltekit and packages

--- docs/src/routes/docs.json.ts
+++ docs/src/routes/docs.json.ts
@@ -1,16 +1,21 @@
 import type { RequestHandler } from '@sveltejs/kit';
 import { version } from '@ignatiusmb/aqua/package.json';
-import { traverse } from 'marqua';
+import { forge, traverse } from 'marqua';
 
 type Metadata = Record<'title', string>;
 type Section = Record<'index' | 'slug' | 'content' | 'path', string>;
 
 export const get: RequestHandler = () => ({
-	body: traverse<Metadata, Section & Metadata>('content', ({ frontMatter, content, filename }) => {
-		if (filename.includes('draft')) return;
-		const path = `docs/content/${filename}.md`;
-		const [index, slug] = filename.split('.')[0].split('-');
-		content = content.replace(/@VERSION/g, `@${version}`);
-		return { index, slug, ...frontMatter, content, path };
-	}),
+	body: traverse(
+		'content',
+		({ frontMatter, content, breadcrumb }) => {
+			const [filename] = breadcrumb.slice(-1);
+			if (filename.includes('draft')) return;
+			const path = `docs/content/${filename}.md`;
+			const [index, slug] = filename.split('.')[0].split('-');
+			content = content.replace(/@VERSION/g, `@${version}`);
+			return { index, slug, ...frontMatter, content, path };
+		},
+		forge.types<Metadata, Section & Metadata>()
+	),
 });
