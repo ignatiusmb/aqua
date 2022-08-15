f62ffcc81eeefe29b862c55c12a1742b099529bb
Ignatius Bagus <ignatius.mbs@gmail.com>
2022-08-15 21:29:24 +0700

    ts script

--- docs/src/routes/index.svelte
+++ docs/src/routes/index.svelte
@@ -1,19 +1,22 @@
-<script context="module">
-	/** @type {import('@sveltejs/kit').Load} */
-	export async function load({ fetch }) {
+<script context="module" lang="ts">
+	import type { Load } from '@sveltejs/kit';
+	export const load: Load = async ({ fetch }) => {
 		const sections = await fetch('/docs.json').then((r) => r.json());
 		return { props: { sections } };
-	}
+	};
 </script>
 
-<script>
-	export let sections;
+<script lang="ts">
+	export let sections: ComponentProps<Docs>['sections'];
+
+	import type { ComponentProps } from 'svelte';
 	import { Docs } from '@mauss/devtools';
 	import MetaHead from '$lib/components/MetaHead.svelte';
 </script>
 
 <MetaHead
 	title="Aqua - Native DOM Enhancements"
-	description="Aqua by Ignatius. An npm package to quickly build websites with predefined styles and components." />
+	description="A package to quickly build websites with predefined styles and components."
+/>
 
 <Docs {sections} repo="ignatiusmb/aqua" />
